759a55d821356f0759fbb8dcba2667f75fbdfec2
Natanael Lima <taelima1997@gmail.com>
2020-05-21 12:12:12 -0300

    created crud projects

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -2,34 +2,18 @@
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+/** @typedef {import('@adonisjs/auth/src/Schemes/Session')} AuthSession */
 
-/**
- * Resourceful controller for interacting with projects
- */
 class ProjectController {
   /**
-   * Show a list of all projects.
-   * GET projects
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @param {AuthSession} ctx.auth
    */
-  async index ({ request, response, view }) {
-  }
+  async index ({ request, auth }) {
+    const projects = request.team.projects().fetch()
 
-  /**
-   * Render a form to be used for creating a new project.
-   * GET projects/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return projects
   }
 
   /**
@@ -38,9 +22,13 @@ class ProjectController {
    *
    * @param {object} ctx
    * @param {Request} ctx.request
-   * @param {Response} ctx.response
+   * @param {AuthSession} ctx.auth
    */
-  async store ({ request, response }) {
+  async store ({ request, auth }) {
+    const data = request.only(['title'])
+    const project = request.team.projects().create(data)
+
+    return project
   }
 
   /**
@@ -49,22 +37,11 @@ class ProjectController {
    *
    * @param {object} ctx
    * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show ({ params, request }) {
+    const projects = request.team.projects().where('id', params.id).fetch()
 
-  /**
-   * Render a form to update an existing project.
-   * GET projects/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+    return projects
   }
 
   /**
@@ -73,9 +50,18 @@ class ProjectController {
    *
    * @param {object} ctx
    * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params, request }) {
+    const data = request.only(['title'])
+    const project = await request.team.projects()
+      .where('id', params.id)
+      .first()
+
+    project.merge(data)
+
+    await project.save()
+
+    return project
   }
 
   /**
@@ -84,9 +70,15 @@ class ProjectController {
    *
    * @param {object} ctx
    * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params, request }) {
+    const project = await request.team.projects()
+      .where('id', params.id)
+      .first()
+
+    await project.delete()
+
+    project.save()
   }
 }
 
