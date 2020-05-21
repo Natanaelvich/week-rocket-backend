d51405598aadd7901489d6c86035f7d7f0a9cdad
Natanael Lima <taelima1997@gmail.com>
2020-05-21 11:48:29 -0300

    created store and session user invited

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,16 +1,39 @@
 'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const User = use('App/Models/User')
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Invite = use('App/Models/Invite')
 
 class UserController {
-  async store ({ request }) {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response, auth }) {
     const { name, email, password } = request.all()
 
-    const user = new User()
-    user.name = name
-    user.email = email
-    user.password = password
+    const teamsQuery = Invite.query().where('email', email)
+    const teams = await teamsQuery.pluck('team_id')
 
-    await user.save()
+    if (teams.length === 0) {
+      return response
+        .status(401)
+        .send({ message: 'You´re not invited to any team' })
+    }
+
+    const user = await User.create({ name, email, password })
+
+    await user.teams().attach(teams)
+
+    await teamsQuery.delete()
+
+    const tocken = await auth.attempt(email, password)
+
+    return tocken
   }
 }
 
