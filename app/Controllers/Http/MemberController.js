c0de83153c6a596d5a7f570a6085b11005312ba3
Natanael Lima <taelima1997@gmail.com>
2020-05-24 11:46:46 -0300

    udpate memberController and UserTeam Model

--- app/Controllers/Http/MemberController.js
+++ app/Controllers/Http/MemberController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const UserTeam = use('App/Models/UserTeam')
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
@@ -11,10 +12,13 @@ class MemberController {
    * @param {Request} ctx.request
    */
   async index ({ request }) {
+    // const members = await UserTeam.QueryBuilder(
+    //   'select `users`.*, `user_team`.`user_id` as `pivot_user_id`, `user_team`.`user_team_id` as `pivot_user_team_id` from `users` inner join `user_team` on `users`.`id` = `user_team`.`user_id` where `user_team`.`user_team_id` in (1)'
+    // )
     const members = await UserTeam.query()
       .where('team_id', request.team.id)
       .with('roles')
-      // .with('user')
+      .with('user')
       .fetch()
 
     return members
